f68cb5138eeced78c5465e7c2b00a0ac33c87ca0
nic00la <nic00la.kaleta@gmail.com>
2024-06-19 23:46:02 +0200

    Details City Page ✨

--- Weather App  ASP.NET/Controllers/HomeController.cs
+++ Weather App  ASP.NET/Controllers/HomeController.cs
@@ -46,4 +46,51 @@ public class HomeController : Controller
 
         return View("Index", cities);
     }
+
+    [Route("/weather/{cityCode}")]
+    public IActionResult Details(string? cityCode)
+    {
+        @ViewBag.Title = "Weather App - Details";
+
+        if (cityCode == null) return NotFound("City code can't be null");
+
+        List<string>? colors = new()
+            { "#6db7e6", "#e1ff8f", "#f9ca70" }; // Example colors
+
+        List<City> cities = new()
+        {
+            new City()
+            {
+                Id = 1,
+                Name = "London",
+                CityUniqueCode = "LND",
+                DateAndTime = DateTime.Parse("2030-01-01 8:00"),
+                Temperature = 28.7,
+                Color = colors[0]
+            },
+            new City()
+            {
+                Id = 2,
+                Name = "New York",
+                CityUniqueCode = "NYC",
+                DateAndTime = DateTime.Parse("2030-01-01 3:00"),
+                Temperature = 33.5,
+                Color = colors[1]
+            },
+            new City()
+            {
+                Id = 3,
+                Name = "Paris",
+                CityUniqueCode = "PAR",
+                DateAndTime = DateTime.Parse("2030-01-01 9:00"),
+                Temperature = 26.8,
+                Color = colors[2]
+            }
+        };
+
+        City? matchingCodeCity =
+            cities.FirstOrDefault(c => c.CityUniqueCode == cityCode);
+
+        return View(matchingCodeCity); // Views/Home/Details.cshtml
+    }
 }
